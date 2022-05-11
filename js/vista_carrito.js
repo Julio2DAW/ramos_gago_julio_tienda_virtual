6e94afe922432f52a44a6fe2d9eca04d414b08bd
Julio2DAW <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2022-05-11 12:29:58 +0200

    validaciones

--- js/vista_carrito.js
+++ js/vista_carrito.js
@@ -4,6 +4,7 @@ export class VistaCarrito{
 
         this.controlador = controlador
         document.getElementById('verProductos').onclick = this.verProductos.bind(this)
+        document.getElementById('idReg').onclick = this.validaciones.bind(this)
     }
     
     mostrar(ver){
@@ -22,36 +23,13 @@ export class VistaCarrito{
         this.controlador.verProductos()
     }
 
-    productosCarrito(productos){
-
-        let cajas = document.createElement('div')
-        divCarrito.appendChild(cajas)
-        cajas.classList.add('cajas')
-
-        for (let producto of productos) {
-
-            let caja = document.createElement('div')
-            cajas.appendChild(caja)
-            caja.classList.add('caja')
-
-            let img = document.createElement('img')
-            caja.appendChild(img)
-            img.src=producto.imagen
-            
-            let p = document.createElement('p')
-            caja.appendChild(p)
-            p.textContent = producto.nombre
-
-            let p2 = document.createElement('p')
-            caja.appendChild(p2)
-            p2.textContent = producto.precio
-
-            let button = document.createElement('button')
-            caja.appendChild(button)
-            button.textContent = 'Añadir'
-            
-            
-            button.onclick = this.anadirCarrito.bind(this, producto) 
-        }
+    validaciones(){
+        console.log('hola');
+        /*
+        let exp1 = document.getElementById("idNIF")
+        let letra = exp1.value[8]
+        let numeros= exp1.value.substring(0, exp1.value.length - 1);
+        let comprobacion=parseInt(numeros)%23
+        */
     }
 }
